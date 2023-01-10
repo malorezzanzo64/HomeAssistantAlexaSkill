667b696306c32920d70765bdc235a95bb0a21ff4
malorezzanzo64 <96385154+malorezzanzo64@users.noreply.github.com>
2023-01-10 22:56:30 +0100

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,34 @@
+module.exports = {
+    /* UPDATE THESE VARIABLES WITH YOUR CONFIG */
+    HOME_ASSISTANT_URL : '',  // REPLACE WITH THE URL FOR YOUR HOME ASSISTANT WITHOUT THE INITIAL HTTPS://
+    VERIFY_SSL : true,  // SET TO FALSE IF YOU DO NOT HAVE VALID CERTS
+    TOKEN : '',  // ADD YOUR LONG LIVED TOKEN IF NEEDED OTHERWISE LEAVE BLANK
+    DEBUG : true,  // SET TO TRUE IF YOU WANT TO SEE MORE DETAILS IN THE LOGS
+    HA_ENTITY : 'var.alexa_event',
+    HA_ENTITY_DEFAULT_DATA : {
+                            "state": "",
+                            "attributes": {
+                                "text": "",
+                                "slots": {}
+                                }
+                            },
+    DIALOGMODEL : false,
+    
+    YES_INTENT_RESPONSE_TYPE : "ResponseYes",
+    YES_INTENT_RESPONSE_SLOT : undefined,
+    
+    NO_INTENT_RESPONSE_TYPE : "ResponseNo",
+    NO_INTENT_RESPONSE_SLOT : undefined,
+    
+    NONE_INTENT_RESPONSE_TYPE : "ResponseNone",
+    
+    SELECT_INTENT_RESPONSE_TYPE : "ResponseSelect",
+    SELECT_INTENT_RESPONSE_SLOT : undefined,
+    
+    NUMBER_INTENT_RESPONSE_TYPE : "ResponseNumeric",
+    NUMBER_INTENT_SLOT : "number",
+    
+    DURATION_INTENT_RESPONSE_TYPE : "ResponseDuration",
+    DURATION_INTENT_RESPONSE_SLOT : "duration"
+    
+}
