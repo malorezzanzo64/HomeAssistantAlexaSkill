0e0045fac59609b1b80ef982c40763602f09185a
malorezzanzo64 <96385154+malorezzanzo64@users.noreply.github.com>
2023-01-10 22:35:26 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+const Alexa = require('ask-sdk-core');
+//var persistenceAdapter = getPersistenceAdapter();
+
+const util = require('./util'); // utility functions
+const interceptors = require('./interceptors');
+const handlers = require('./handlers');
+
+
+/**
+ * This handler acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom 
+ * */
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        handlers.HALaunchRequestHandler,
+        handlers.HAIntentHandler,
+        handlers.LaunchRequestHandler,
+        handlers.IntentHandler,
+        handlers.HelpIntentHandler,
+        handlers.CancelAndStopIntentHandler,
+        handlers.FallbackIntentHandler,
+        handlers.SessionEndedRequestHandler,
+        handlers.IntentReflectorHandler
+    )
+    .addRequestInterceptors(
+        interceptors.LocalisationRequestInterceptor,
+        interceptors.HaEntityRequestInterceptor,
+        interceptors.EditHandlerInputRequestInterceptor,
+        interceptors.CheckOriginRequestInterceptor,
+        interceptors.LoggingRequestInterceptor
+    )
+    .addResponseInterceptors(
+        interceptors.LoggingResponseInterceptor
+    )
+    .addErrorHandlers(
+        handlers.ErrorHandler
+        //SaveAttributesResponseInterceptor
+    )
+    //.withPersistenceAdapter(persistenceAdapter)
+    .withCustomUserAgent('HomeAssistant/NodeJS')
+    .lambda();
