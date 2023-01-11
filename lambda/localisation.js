082c78b497cdb8a0eeaf83472d71a949340071e7
malorezzanzo64 <96385154+malorezzanzo64@users.noreply.github.com>
2023-01-11 04:27:08 +0100

    Create localisation.js

---
+++ lambda/localisation.js
@@ -0,0 +1,67 @@
+module.exports = {
+    en: {
+        translation: {
+            WELCOME_MSG: 'Welcome, you can say Hello or Help. Which would you like to try?',
+            HELLO_MSG: 'Hello World!',
+            HELP_MSG: 'You can say hello to me! How can I help?',
+            GOODBYE_MSG: 'Goodbye!',
+            REFLECTOR_MSG: 'You just triggered {{intent}}',
+            FALLBACK_MSG: 'Sorry, I don\'t know about that. Please try again.',
+            ERROR_MSG: 'Sorry, there was an error. Please try again.',
+            INSULT_MSG: 'You selected an insult',
+            OK_MSG: 'Ok',
+            YES_INTENT_RESPONSE: 'Ok',
+            NO_INTENT_RESPONSE: 'Ok',
+            SELECT_INTENT_RESPONSE: 'You selected {{select}}',
+        }
+    },
+    it : {
+        translation: {
+            WELCOME_MSG: 'Benvenuto',
+            HELLO_MSG: 'Ciao',
+            HELP_MSG: 'Hai bisogno di aiuto?',
+            GOODBYE_MSG: 'Ciao',
+            REFLECTOR_MSG: 'Hai appena lanciato {{intent}}',
+            FALLBACK_MSG: 'Credo di non aver capito',
+            ERROR_MSG: 'C\'è stato un errore',
+            INSULT_MSG: 'Hai selezionato un insulto',
+            OK_MSG: 'Ok',
+            YES_INTENT_RESPONSE: 'Ok',
+            NO_INTENT_RESPONSE: 'Ok',
+            SELECT_INTENT_RESPONSE: 'You selected {{select}}',
+        }
+    },
+    es: {
+        translation: {
+            WELCOME_MSG: 'Bienvenido, puedes decir Hola o Ayuda. Cual prefieres?',
+            HELLO_MSG: 'Hola Mundo!',
+            HELP_MSG: 'Puedes decirme hola. Cómo te puedo ayudar?',
+            GOODBYE_MSG: 'Hasta luego!',
+            REFLECTOR_MSG: 'Acabas de activar {{intent}}',
+            FALLBACK_MSG: 'Lo siento, no se nada sobre eso. Por favor inténtalo otra vez.',
+            ERROR_MSG: 'Lo siento, ha habido un error. Por favor inténtalo otra vez.'
+        }
+    },
+    fr:{
+        translation: {
+            WELCOME_MSG: 'Bienvenue sur le génie des salutations, dites-moi bonjour et je vous répondrai',
+            HELLO_MSG: 'Bonjour à tous!',
+            HELP_MSG: 'Dites-moi bonjour et je vous répondrai!',
+            GOODBYE_MSG: 'Au revoir!',
+            REFLECTOR_MSG: 'Vous avez invoqué l\'intention {{intent}}',
+            FALLBACK_MSG: 'Désolé, je ne sais pas. Pouvez-vous reformuler?',
+            ERROR_MSG: 'Désolé, je n\'ai pas compris. Pouvez-vous reformuler?'
+        }
+    },
+    pt:{
+        translation: {
+            WELCOME_MSG: 'Olá, você pode dizer olá ou ajuda. Quais deles voce deseja testar?',
+            HELLO_MSG: 'Olá!',
+            HELP_MSG: 'Você pode dizer olá para mim! Como posso ajudar?',
+            GOODBYE_MSG: 'Até logo!',
+            REFLECTOR_MSG: 'Você acaba de iniciar {{intent}}',
+            FALLBACK_MSG: 'Desculpe, não sei nada sobre isso. Por favor tente novamente.',
+            ERROR_MSG: 'Desculpe, occoreu um erro. Por favor tente novamtne.'
+        }
+    }
+}
