f5f64ab48145a1ec13893f6b7f99187334aeccf6
malorezzanzo64 <96385154+malorezzanzo64@users.noreply.github.com>
2023-10-03 20:17:16 +0200

    Update constants.js

--- lambda/constants.js
+++ lambda/constants.js
@@ -1,61 +1,58 @@
 module.exports = {
     /* UPDATE THESE VARIABLES WITH YOUR CONFIG */
-    HOME_ASSISTANT_URL : '',  // REPLACE WITH THE URL FOR YOUR HOME ASSISTANT
+    HOME_ASSISTANT_URL : 'ha.malorezzanzo.com',  // REPLACE WITH THE URL FOR YOUR HOME ASSISTANT
     VERIFY_SSL : true,  // SET TO FALSE IF YOU DO NOT HAVE VALID CERTS
-    TOKEN : '',  // ADD YOUR LONG LIVED TOKEN IF NEEDED OTHERWISE LEAVE BLANK
+    TOKEN : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI3NTYzZjlhNzFmNmE0OTBhOTZkOWNhMWQ3YzY5NzFmYyIsImlhdCI6MTY3MjE4ODQ2MywiZXhwIjoxOTg3NTQ4NDYzfQ.-keI9UwvrZSIpLK2bi5-bC0flF1oQ1rO2dX_nKrt10I',  // ADD YOUR LONG LIVED TOKEN IF NEEDED OTHERWISE LEAVE BLANK
     DEBUG : true,  // SET TO TRUE IF YOU WANT TO SEE MORE DETAILS IN THE LOGS
-    HA_ENTITY : 'sensor.alexa_event',
-    HA_ENTITY_DEFAULT_DATA : {
-                            "state": "alexa_event", // event to create
-                            "attributes": {
-                                "text": "",
-                                "id": "",
-                                "intent": "LaunchRequest",
-                                "slots": {}
-                                }
-                            },
-    DIALOGMODEL : false,
-    
-    YES_INTENT_RESPONSE_TYPE : "ResponseYes",
-    YES_INTENT_RESPONSE_SLOT : undefined,
-    YES_INTENT_RESPONSE_FROM_HA : true,
-    
-    NO_INTENT_RESPONSE_TYPE : "ResponseNo",
-    NO_INTENT_RESPONSE_SLOT : undefined,
-    NO_INTENT_RESPONSE_FROM_HA : true,
-
-    NONE_INTENT_RESPONSE_TYPE : "ResponseNone",
-    NONE_INTENT_RESPONSE_FROM_HA : true,
-
-    SELECT_INTENT_RESPONSE_TYPE : "ResponseSelect",
-    SELECT_INTENT_RESPONSE_SLOT : undefined,
-    SELECT_INTENT_RESPONSE_FROM_HA : true,
-
-    NUMBER_INTENT_RESPONSE_TYPE : "ResponseNumeric",
-    NUMBER_INTENT_SLOT : "number",
-    NUMBER_INTENT_RESPONSE_FROM_HA : true,
-
-    DURATION_INTENT_RESPONSE_TYPE : "ResponseDuration",
-    DURATION_INTENT_RESPONSE_SLOT : "duration",
-    DURATION_INTENT_RESPONSE_FROM_HA : true,
     
     
-    SELECTROOM_INTENT_RESPONSE_TYPE : "ResponseRoom",
-    SELECTROOM_INTENT_RESPONSE_SLOT : "room",
-    SELECTROOM_INTENT_RESPONSE_FROM_HA : true,
+    // the Home Assistant entities that this skill queries
+    HA_ENTITY : {
+        EVENT : 'input_text.alexa_event',
+        EVENT_DEFAULT : 'alexa_event',
+        TEXT : 'input_text.alexa_event_text',
+        TEXT_DEFAULT : '',
+        ID : 'input_text.alexa_event_id',
+        ID_DEFAULT : 'voice_request',
+        SLOTS : 'input_text.alexa_event_slots',
+        SLOTS_DEFAULT : {},
+        DATE : 'input_text.alexa_date'
+    },
     
-    SELECTTASK_INTENT_RESPONSE_TYPE : "ResponseTask",
-    SELECTTASK_INTENT_RESPONSE_SLOT : "task",
-    SELECTTASK_INTENT_RESPONSE_FROM_HA : true,
-
-    SELECTPERSON_INTENT_RESPONSE_TYPE : "ResponsePerson",
-    SELECTPERSON_INTENT_RESPONSE_SLOT : "person",
-    SELECTPERSON_INTENT_RESPONSE_FROM_HA : true
+    INTENT : {
+        // the respose type to sendo to Home Assistant
+        RESPONSE_TYPE : {
+            DEFAULT : "ResponseOther",
+            NONE : "ResponseNone",
+            YES : "ResponseYes",
+            NO : "ResponseNo",
+            SELECT : "ResponseSelect",
+            SELECTION : "ResponseSelection",
+            NUMBER : "ResponseNumeric",
+            DURATION : "ResponseDuration",
+        },
+        
+        RESPONSE_SLOT : {
+            DEFAULT : undefined,
+            NONE : undefined,
+            YES : undefined,
+            NO : undefined,
+            SELECT : "selection",
+            SELECTION : "selection",
+            NUMBER : "number",
+            DURATION : "duration"
+        },
+        
+        // if the selected intent response should come from Home Assistant
+        RESPONSE_FROM_HA : {
+            DEFAULT : false,
+            NONE : true,
+            YES : true,
+            NO : true,
+            SELECT : true,
+            SELECTION : true,
+            NUMBER : true,
+            DURATION : true,
+        },
+    }
 }
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-
-    Terms
-    Privacy
-    S
