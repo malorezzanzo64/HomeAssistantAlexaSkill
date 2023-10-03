85803ab768be1b36eb89588cec008a3a190397af
malorezzanzo64 <96385154+malorezzanzo64@users.noreply.github.com>
2023-10-03 20:20:06 +0200

    Update interceptors.js

--- lambda/interceptors.js
+++ lambda/interceptors.js
@@ -7,22 +7,31 @@ const constants = require('./constants');
 // This request interceptor will log all incoming requests to this lambda
 const LoggingRequestInterceptor = {
     process(handlerInput) {
+        console.log("~~~~ Interceptor: LoggingRequestInterceptor");
         console.log(`Incoming request: ${JSON.stringify(handlerInput.requestEnvelope)}`);
-        console.log(`HA Launch Request: ${handlerInput.attributesManager.getSessionAttributes().launchedByHA}, ${Alexa.isNewSession(handlerInput.requestEnvelope)}, ${Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest'}`);
+        console.log(`Incoming Request Info:
+            Launched by HA: ${handlerInput.attributesManager.getSessionAttributes().launchedByHA};
+            New Session: ${Alexa.isNewSession(handlerInput.requestEnvelope)};
+            Launch Request: ${Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest'}`);
     }
 };
 
 // This response interceptor will log all outgoing responses of this lambda
 const LoggingResponseInterceptor = {
     process(handlerInput, response) {
-        console.log(`Outgoing response: ${JSON.stringify(response)}`);
-        console.log(`HA Launch Request: ${handlerInput.attributesManager.getSessionAttributes().launchedByHA}, ${Alexa.isNewSession(handlerInput.requestEnvelope)}, ${Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest'}`);
+        console.log("~~~~ Interceptor: LoggingResponseInterceptor");
+        console.log(`Outgoing Response: ${JSON.stringify(response)}`);
+        console.log(`Outgoing Response Info:
+            Launched by HA: ${handlerInput.attributesManager.getSessionAttributes().launchedByHA};
+            New Session: ${Alexa.isNewSession(handlerInput.requestEnvelope)};
+            Launch request: ${Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest'}`);
     }
 };
 
 // This request interceptor will bind a translation function 't' to the handlerInput
 const LocalisationRequestInterceptor = {
     process(handlerInput) {
+        console.log("~~~~ Interceptor: LocalisationRequestInterceptor");
         i18n.init({
             lng: Alexa.getLocale(handlerInput.requestEnvelope),
             resources: languageStrings
@@ -36,12 +45,26 @@ const LocalisationRequestInterceptor = {
 // This request interceptors saves the Home Assistant entity in session attributes and then resets it to the default value in Home Assistant
 const HaEntityRequestInterceptor = {
     async process(handlerInput) {
+        console.log("~~~~ Interceptor: HaEntityRequestInterceptor");
+        // if there's a date slot: it's needed to first check if it is an intent request otherwise it throws an error
+        if(Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getSlot(handlerInput.requestEnvelope, 'date')
+        ){
+            logic.setHaEntity(constants.HA_ENTITY.DATE, Alexa.getSlotValue(handlerInput.requestEnvelope, 'date'));
+        }
         let sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
         if (sessionAttributes.haEntity === undefined) {  // Alexa.isNewSession(handlerInput.requestEnvelope) doesn't work because if the dialog is delegated the skill will never receive a new session request
-            var haEntity = await logic.getHaEntity();
+            var haEntity = {};
+            haEntity.text = await logic.getHaEntity(constants.HA_ENTITY.TEXT);
+            haEntity.id = await logic.getHaEntity(constants.HA_ENTITY.ID);
+            haEntity.event = await logic.getHaEntity(constants.HA_ENTITY.EVENT);
+            haEntity.slots = await logic.getHaEntity(constants.HA_ENTITY.SLOTS);
             logic.SaveHaEntityInSessionAttributes(handlerInput, haEntity);
             // set ha entity to the default state
-            logic.setHaEntity();
+            logic.setHaEntity(constants.HA_ENTITY.TEXT, constants.HA_ENTITY.TEXT_DEFAULT);
+            logic.setHaEntity(constants.HA_ENTITY.EVENT, constants.HA_ENTITY.EVENT_DEFAULT);
+            logic.setHaEntity(constants.HA_ENTITY.SLOTS, constants.HA_ENTITY.SLOTS_DEFAULT);
+            logic.setHaEntity(constants.HA_ENTITY.ID, constants.HA_ENTITY.ID_DEFAULT);
         }
     }
 }
@@ -49,13 +72,18 @@ const HaEntityRequestInterceptor = {
 // This request handler set the session attribute launchedByHa true or false
 const CheckOriginRequestInterceptor = {
     async process(handlerInput) {
+        console.log("~~~~ Interceptor: CheckOriginRequestInterceptor");
         logic.isLaunchedByHa(handlerInput);
     }
 }
 
 const EditHandlerInputRequestInterceptor = {
     async process(handlerInput) {
-        if (!(Alexa.isNewSession(handlerInput.requestEnvelope)) && logic.isLaunchedByHa(handlerInput)) {
+        if (!(Alexa.isNewSession(handlerInput.requestEnvelope))
+            && logic.isLaunchedByHa(handlerInput)
+            && Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+        ) {
+            console.log("~~~~ Interceptor: EditHandlerInputRequestInterceptor");
             logic.EditHandlerInput(handlerInput);
         }
     }
@@ -64,13 +92,23 @@ const EditHandlerInputRequestInterceptor = {
 
 const PostHaEventResponseInterceptor = {
     process(handlerInput) {
-        if (!(Alexa.isNewSession(handlerInput.requestEnvelope))
-            && Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            ) {
-            let intentName = (Alexa.getIntentName(handlerInput.requestEnvelope)).replace("AMAZON.","").replace("Intent","").toUpperCase();
-            let responseType = eval("constants." + intentName + "_INTENT_RESPONSE_TYPE");
-            let responseSlot = eval("constants." + intentName + "_INTENT_RESPONSE_SLOT");
-            // let response = Alexa.getSlotValue(handlerInput.requestEnvelope, responseSlot);
+        if (Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' || Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest') {
+            console.log("~~~~ Interceptor: PostHaEventResponseInterceptor");
+            var responseType;
+            var responseSlot;
+            if(Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest') {
+                let intentName = (Alexa.getIntentName(handlerInput.requestEnvelope)).replace("AMAZON.","").replace("Response","").replace("Intent","").toUpperCase();
+                console.log("INTENT NAME: " + intentName);
+                // responseType = eval("constants." + intentName + "_INTENT_RESPONSE_TYPE");
+                responseType = constants.INTENT.RESPONSE_TYPE[intentName];
+                console.log("RESPONSE TYPE: " + responseType);
+                responseSlot = constants.INTENT.RESPONSE_SLOT[intentName];
+                console.log("RESPONSE SLOT: " + responseSlot);
+                // let response = Alexa.getSlotValue(handlerInput.requestEnvelope, responseSlot);
+            } else {
+                responseType = constants.INTENT.RESPONSE_TYPE.NONE;
+                responseSlot = constants.INTENT.RESPONSE_SLOT.NONE;
+            }
             logic.postHaEvent(handlerInput, responseSlot, responseType);
         }
     }
